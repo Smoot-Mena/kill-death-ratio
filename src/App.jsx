4c9dd5363afe66945771d513bacd05bde0bbcd25
Smoot-Mena <Msmoot810@gmail.com>
2025-04-06 20:18:31 -0400

    Adds Home page and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
-    <section>This is the app.</section>
+    <section>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </section>
   )
 }
 
